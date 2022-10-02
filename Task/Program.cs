d49765107aa9e5a6efde35bf16cc13ba340c1d50
mila <milavoronina1997@mail.ru>
2022-10-03 01:06:46 +0500

    введение корректировок в метод с описанием поиска трех элементов

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,22 +1,27 @@
 ﻿Console.WriteLine("Введите элементы массива через пробел или запятую: ");
 string x = Console.ReadLine();
 string[] result = x.Split(';',' ',',','.').ToArray();
+Console.ReadKey();
 string[] result2 = new string[result.Length];
 void GetThreeSymbolsElemnets(string[] array1,string[] array2)
 {
     for(int i = 0 ; i < array1.Length; i++)
     {
-        if(array1[i].Length <= 3) array2[i] = array1[i];
+        if(array1[i].Length <= 3) 
+        {
+            array2[i] = array1[i];
+        }
     }
 }
 void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
-        Console.Write($" '{array[i]}' ");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
 GetThreeSymbolsElemnets(result,result2);
 Console.WriteLine("Элементы, состоящие из трех символов: ");
 PrintArray(result2);
+
