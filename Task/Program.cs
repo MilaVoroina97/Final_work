345e960ca1f8891937b2644f9aa1babfff786fe8
mila <milavoronina1997@mail.ru>
2022-10-03 00:51:39 +0500

    создали два массива и создали возможность для данных пользователем

---
+++ Task/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите элементы массива через пробел или запятую: ");
+string x = Console.ReadLine();
+string[] result = x.Split(';',' ',',','.').ToArray();
+string[] result2 = new string[result.Length];
