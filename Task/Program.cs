f5a6362a72dce2102b71fd4e4d45885dbd21b88f
mila <milavoronina1997@mail.ru>
2022-10-03 00:53:16 +0500

    описали метод для нахождения элементов с тремя символами

--- Task/Program.cs
+++ Task/Program.cs
@@ -2,3 +2,10 @@
 string x = Console.ReadLine();
 string[] result = x.Split(';',' ',',','.').ToArray();
 string[] result2 = new string[result.Length];
+void GetThreeSymbolsElemnets(string[] array1,string[] array2)
+{
+    for(int i = 0 ; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3) array2[i] = array1[i];
+    }
+}
